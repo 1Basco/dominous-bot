9b130b376321905de61612ef25a55599dc1d49b0
1Basco <lsos13@gmail.com>
2023-11-24 16:43:21 -0300

    feat: add check for bucha

--- src/bot.ts
+++ src/bot.ts
@@ -15,6 +15,7 @@ export default function decidePlay(gameData: GameData) {
   }
   const picks = getHandAllPossiblePicks(gameData);
   const pick = getHighestValuePick(picks);
+
   return pick ?? {};
 }
 
@@ -37,10 +38,11 @@ function getTableSides(table: Array<String>) {
 function getHandAllPossiblePicks(gameData: GameData) {
   const sides = getTableSides(gameData.mesa);
 
-  // search mao
   const picks: ({ pedra: string; lado: string } | undefined)[] = gameData.mao
     .map((piece: string) => {
+      /*@ts-ignore*/
       const firstSide = sides.first[0];
+      /*@ts-ignore*/
       const lastSide = sides.last[2];
 
       if (piece[0] === firstSide || piece[2] === firstSide) {
@@ -66,6 +68,16 @@ function getHandAllPossiblePicks(gameData: GameData) {
 function getHighestValuePick(
   picks: ({ pedra: string; lado: string } | undefined)[]
 ) {
+  const bucha: {
+    pedra: string;
+    lado: string;
+  } | null = getBucha(picks);
+  if (bucha !== null) {
+    return {
+      pedra: bucha.pedra,
+      lado: bucha.lado,
+    };
+  }
   const picksWithValue = picks.map((pick: any) => ({
     pedra: pick.pedra,
     value: parseInt(pick.pedra[0]) + parseInt(pick.pedra[2]),
@@ -77,6 +89,37 @@ function getHighestValuePick(
     (pick: any) => pick.value === maxValue
   );
 
+  if (highestValuePicks.length === 0) {
+    return;
+  }
+  return {
+    pedra: highestValuePicks[0].pedra,
+    lado: highestValuePicks[0].lado,
+  };
+}
+
+function getBucha(picks: ({ pedra: string; lado: string } | undefined)[]) {
+  const buchas: ({ pedra: string; lado: string } | undefined)[] = [];
+
+  picks.forEach((pick) => {
+    pick?.pedra[0] === pick?.pedra[2] ? buchas.push(pick) : null;
+  });
+
+  const picksWithValue = buchas.map((pick: any) => ({
+    pedra: pick.pedra,
+    value: parseInt(pick.pedra[0]) + parseInt(pick.pedra[2]),
+    lado: pick.lado,
+  }));
+
+  const maxValue = Math.max(...picksWithValue.map((pick: any) => pick.value));
+  const highestValuePicks = picksWithValue.filter(
+    (pick: any) => pick.value === maxValue
+  );
+  console.log("highestValuePicks: ", highestValuePicks);
+  if (highestValuePicks.length === 0) {
+    console.log("sem bucha");
+    return null;
+  }
   return {
     pedra: highestValuePicks[0].pedra,
     lado: highestValuePicks[0].lado,
